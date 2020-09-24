06ddf6f9b641ed7213d6ddb25169aee4c54bbd12
kalana <kalana.mihiranga@hotmail.com>
2020-09-24 23:26:01 +0530

    create add newtodo function

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>ToDo List</h1>
+  <form @submit.prevent="addNewToDos">
+    <input type="text" placeholder="Enter title...." />
+    <button type="submit">Add ToDo</button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  setup() {
+    const newToDo = ref("");
+    const todos = ref([]);
+
+    function addNewToDos() {
+      todos.value.push({
+        id: Date.now(),
+        done: false,
+        content: newToDo.value,
+      });
+    }
+  },
+};
+</script>
