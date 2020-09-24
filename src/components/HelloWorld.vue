3956edd860a5998714d486b8d7f0a4b7e251782a
kalana <kalana.mihiranga@hotmail.com>
2020-09-24 23:38:55 +0530

    make todos ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,37 @@
 <template>
-  <h1>ToDo List</h1>
-  <form @submit.prevent="addNewToDos">
-    <input type="text" placeholder="Enter title...." />
-    <button type="submit">Add ToDo</button>
-  </form>
+  <div class="album py-5 bg-light">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="card mb-12 shadow-sm">
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <input
+                    type="text"
+                    class="form-control"
+                    id="new_todo"
+                    placeholder="Enter new todo...."
+                    v-model="newToDo"
+                  />
+                </div>
+                <button type="submit" class="btn btn-primary">ADD NEW TODO</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-12">
+          <div v-for="(todo, index) in todos" :key="index" class="card mb-8 shadow-sm">
+            <div class="card-body">
+              <h3>{{todo.content}}</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -11,8 +39,7 @@ export default {
   name: "HelloWorld",
   setup() {
     const newToDo = ref("");
-    const todos = ref([]);
-
+    // const todos = ref([]);
     function addNewToDos() {
       todos.value.push({
         id: Date.now(),
@@ -20,6 +47,11 @@ export default {
         content: newToDo.value,
       });
     }
+    return {
+      newToDo,
+      todos,
+      addNewToDos,
+    };
   },
 };
 </script>
